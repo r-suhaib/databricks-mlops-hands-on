1e761d2db9846f67ad23b2ce67c9d1653d7281bd
Suhaibur Rehman <Suhaibur.Rehman@thameswater.co.uk>
2026-08-03 14:14:23 +0100

    estabilished registry driven promotion and registry utils.py

---
+++ docs/registry_driven_promotion.md
@@ -0,0 +1,22 @@
+## Current State
+
+Champion metric hardcoded.
+
+## Target State
+
+Champion metric retrieved from:
+
+Model Registry
+    ↓
+Champion Alias
+    ↓
+Model Version
+    ↓
+MLflow Run
+    ↓
+Accuracy Metric
+
+## Purpose
+
+Enable fully automated
+Champion/Challenger comparisons.
---
+++ src/evaluation/registry_utils.py
@@ -0,0 +1,28 @@
+from mlflow import MlflowClient
+
+
+def get_champion_run_id(
+    model_name: str
+):
+
+    client = MlflowClient()
+
+    champion = client.get_model_version_by_alias(
+        model_name,
+        "Champion"
+    )
+
+    return champion.run_id
+
+def get_run_metric(
+    run_id: str,
+    metric_name: str
+):
+
+    client = MlflowClient()
+
+    run = client.get_run(run_id)
+
+    return run.data.metrics.get(
+        metric_name
+    )
